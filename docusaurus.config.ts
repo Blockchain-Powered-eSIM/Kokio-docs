382eeb6a27a478d565fccd096e1cb1e34e4ec737
GuyPhy <guyphy@gmms.xyz>
2025-01-28 18:11:20 +0530

    Change links

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -8,14 +8,14 @@ const config: Config = {
   favicon: 'images/KokioLogo',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://docs.kokio.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
+  organizationName: 'Kokio', // Usually your GitHub org/user name.
   projectName: 'Kokio', // Usually your repo name.
 
   onBrokenLinks: 'throw',
